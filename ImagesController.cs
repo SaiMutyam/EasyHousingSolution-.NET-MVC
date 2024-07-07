5e86417a2d813cde180f1aa05d8ff57322bd955a
Sai mutyam Manda <118879600+SaiMutyam@users.noreply.github.com>
2024-07-08 02:00:29 +0530

    Add files via upload
    controllers

---
+++ ImagesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EasyhousingSolution_WebAPI.Model;
+
+namespace EasyhousingSolution_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly Ehs_DbContext _context;
+
+        public ImagesController(Ehs_DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Images
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Images>>> GetImages()
+        {
+            return await _context.Images.ToListAsync();
+        }
+
+        // GET: api/Images/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Images>> GetImages(int id)
+        {
+            var images = await _context.Images.FindAsync(id);
+
+            if (images == null)
+            {
+                return NotFound();
+            }
+
+            return images;
+        }
+
+        // PUT: api/Images/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutImages(int id, Images images)
+        {
+            if (id != images.ImageId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(images).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ImagesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Images
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Images>> PostImages(Images images)
+        {
+            _context.Images.Add(images);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetImages", new { id = images.ImageId }, images);
+        }
+
+        // DELETE: api/Images/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Images>> DeleteImages(int id)
+        {
+            var images = await _context.Images.FindAsync(id);
+            if (images == null)
+            {
+                return NotFound();
+            }
+
+            _context.Images.Remove(images);
+            await _context.SaveChangesAsync();
+
+            return images;
+        }
+
+        private bool ImagesExists(int id)
+        {
+            return _context.Images.Any(e => e.ImageId == id);
+        }
+    }
+}
---
+++ SellersController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EasyhousingSolution_WebAPI.Model;
+
+namespace EasyhousingSolution_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellersController : ControllerBase
+    {
+        private readonly Ehs_DbContext _context;
+
+        public SellersController(Ehs_DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Sellers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Seller>>> GetSeller()
+        {
+            return await _context.Seller.ToListAsync();
+        }
+
+        // GET: api/Sellers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Seller>> GetSeller(int id)
+        {
+            var seller = await _context.Seller.FindAsync(id);
+
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            return seller;
+        }
+
+        // PUT: api/Sellers/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSeller(int id, Seller seller)
+        {
+            if (id != seller.SellerId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(seller).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SellerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Sellers
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Seller>> PostSeller(Seller seller)
+        {
+            _context.Seller.Add(seller);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSeller", new { id = seller.SellerId }, seller);
+        }
+
+        // DELETE: api/Sellers/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Seller>> DeleteSeller(int id)
+        {
+            var seller = await _context.Seller.FindAsync(id);
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            _context.Seller.Remove(seller);
+            await _context.SaveChangesAsync();
+
+            return seller;
+        }
+
+        private bool SellerExists(int id)
+        {
+            return _context.Seller.Any(e => e.SellerId == id);
+        }
+    }
+}
